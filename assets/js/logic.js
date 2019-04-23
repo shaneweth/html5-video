586c886212c1e69fd2c24305814e9ff21c8cb39a
willow <shaneweth77@gmail.com>
2019-04-23 15:00:12 -0400

    added complete transport fn. fullscreen not working

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -19,16 +19,53 @@ function togglePlay() {
 }
 
 function updateButton() {
-    console.log("Update the button");
+    const icon = this.paused ? "►" : "❚ ❚";
+    console.log(icon);
+    toggle.textContent = icon;
 }
 
+// Skip Function
+
+function skip() {
+    console.log(this.dataset.skip);
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+    console.log(this.name);
+    console.log(this.value);
+}
+
+function handleProgress() {
+    // updates the flex-basis of .progress_filled
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+    console.log(e);
+}
 // event listeners
-
-
 
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
 
+// listens for timeupdate for the handleProgress fn
+video.addEventListener("timeupdate", handleProgress);
 
 toggle.addEventListener("click", togglePlay);
+
+skipButtons.forEach(button => button.addEventListener("click", skip));
+
+ranges.forEach(range => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach(range => range.addEventListener("mousemove", handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => mousedown = true);
+progress.addEventListener("mouseup", () => mousedown = false);
